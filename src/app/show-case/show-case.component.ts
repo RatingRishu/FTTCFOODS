0ab6fc2c9132d1c13f25affa31d00ea949920951
RatingRishu <rairishabh5001@gmail.com>
2025-08-17 18:32:13 +0530

    review

--- src/app/show-case/show-case.component.ts
+++ src/app/show-case/show-case.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './show-case.component.css'
 })
 export class ShowCaseComponent {
-
+ reviews = [
+    {
+      name: 'Amit Sharma',
+      date: '12 Aug 2025',
+      rating: 5,
+      comment: 'Amazing quality! The rice is fresh and aromatic.',
+      avatar: 'https://i.pravatar.cc/50?img=1'
+    },
+    {
+      name: 'Priya Singh',
+      date: '10 Aug 2025',
+      rating: 4,
+      comment: 'Good packaging and fast delivery. Will order again!',
+      avatar: 'https://i.pravatar.cc/50?img=2'
+    },
+    {
+      name: 'Rahul Verma',
+      date: '08 Aug 2025',
+      rating: 5,
+      comment: 'Authentic taste, feels like homegrown rice.',
+      avatar: 'https://i.pravatar.cc/50?img=3'
+    }
+  ];
 }
